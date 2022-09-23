6bb91b32e269c62573e4638f39e93074f59a392b
Jullian Pepito <pepitoj@amazon.com>
2022-09-23 11:22:20 -0700

    Did some cool changes

--- lib/cdk_pipeline.ts
+++ lib/cdk_pipeline.ts
@@ -12,7 +12,7 @@ export class CdkPipelineDemoStack extends Stack {
 
     // Initialize the pipeline
     const cdkPipeline = new cdk_pip.CodePipeline(this, "CDKPipeline", {
-      crossAccountKeys: true, // Allow cross region/account deployment
+      // crossAccountKeys: true, // Allow cross region/account deployment
       synth: new cdk_pip.ShellStep('SynthesizeStep', {
         input: cdk_pip.CodePipelineSource.connection('pepitoj-amzn/cdk-pipelines-class-demo', 'master', {
           connectionArn: 'arn:aws:codestar-connections:us-west-2:848135204948:connection/5ba7c6b8-2a9d-4b53-8c96-adf5ae0eeab5',
@@ -24,21 +24,25 @@ export class CdkPipelineDemoStack extends Stack {
       })
     })
 
-    cdkPipeline.addStage(new SNSStage(this, "SNSStage", {
+    cdkPipeline.addStage(new SNSStage(this, "SNSStage1", {
         env: {
-            region: "us-east-1"
+            region: "us-west-2"
         }
       })
     )
+    cdkPipeline.addStage(new SNSStage(this, "SNSStage2", {
+      env: {
+        region: "us-west-2"
+    }
+    }))
   }
 }
 
 class SNSStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps){
         super(scope, id, props)
-
-        new TopicsStack(this, "MyTopicsStack")
-
+        new TopicsStack(this, "MyTopicsStack1")
+        new TopicsStack(this, "MyTopicsStack2")
     }
 }
 
@@ -52,3 +56,26 @@ class TopicsStack extends Stack {
         })
     }
 }
+
+/**
+ * 1. CDK Piplines are just normal Codepipeliens
+ * 2. The way we use them is to simply initialize the Codepipeline object from aws_cdk_lib.pipelines
+ * 3. They will always come with the following stages already defined:
+ *    - Source
+ *    - 2 "Build" stages
+ *      - Synthesis Step (aka "cdk synth")
+ *      - SelfMutation Step (aka "cdk deploy")
+ *    - Assets (maybe)
+ *      - Appears if we have Assets within our app
+ *    - Many "Application" stages
+ *      - Abstractions of groups of CFN Stacks deployed together
+ */
+
+/**
+"SomePipeline" -|
+    "Stacks that we want to deploy" (i.e. Stages) -|
+      Stack1
+      Stack2
+      Stack3
+      .....
+ */
