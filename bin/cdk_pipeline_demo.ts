19c4df9f3db568989161cc0614539804ad3d8e23
Jullian Pepito <pepitoj@amazon.com>
2022-09-22 23:44:54 -0700

    Updated files

--- bin/cdk_pipeline_demo.ts
+++ bin/cdk_pipeline_demo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineDemoStack } from '../lib/cdk_pipeline_demo-stack';
+import { CdkPipelineDemoStack } from '../lib/cdk_pipeline';
 
 const app = new cdk.App();
 new CdkPipelineDemoStack(app, 'CdkPipelineDemoStack', {
