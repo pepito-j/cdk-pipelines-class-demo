a117dd11ce29e890753a219403ed2e8f437bcb76
Jullian Pepito <pepitoj@amazon.com>
2022-11-08 23:53:26 -0800

    DOing more testing

--- bin/cdk_pipeline_lab.ts
+++ bin/cdk_pipeline_lab.ts
@@ -6,7 +6,7 @@ import { CdkPipelineStack } from '../lib/cdk_pipeline_lab-stack';
 const app = new cdk.App();
 new CdkPipelineStack(app, 'CdkPipelineLabStack', {
   env: {
-    account: 'XXXXXX',
-    region: 'us-east-2'
+    account: '848135204948',
+    region: 'us-west-2'
   }
 });
--- lib/cdk_pipeline_lab-stack.ts
+++ lib/cdk_pipeline_lab-stack.ts
@@ -10,14 +10,19 @@ export class CdkPipelineStack extends Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
         pipelineName: 'MyPipeline',
         synth: new ShellStep('Synth', {
-          input: CodePipelineSource.connection('<GITHUB_USERNAME>/<GITHUB_REPO_NAME>', '<BRANCH_NAME>', {
-            connectionArn: '<CODE_STAR_CONNECTION_ARN>'
+          // input: CodePipelineSource.connection('<GITHUB_USERNAME>/<GITHUB_REPO_NAME>', '<BRANCH_NAME>', {
+          //   connectionArn: '<CODE_STAR_CONNECTION_ARN>'
+          // }),
+          input: CodePipelineSource.connection('pepito-j/cdk-pipelines-class-demo', 'master', {
+            connectionArn: 'arn:aws:codestar-connections:us-west-2:848135204948:connection/5ba7c6b8-2a9d-4b53-8c96-adf5ae0eeab5'
           }),
           commands: ['npm ci', 'npm run build', 'npx cdk synth']
         })
       });
 
       pipeline.addStage(new MyPipelineAppStage(this, "MyPipelineAppStage"));
-    
+      const wave = pipeline.addWave("MyParallelStages")
+      wave.addStage(new MyPipelineAppStage(this, "MyPipelineAppStage1"))
+      wave.addStage(new MyPipelineAppStage(this, "MyPipelineAppStage2"))
     }
 }
